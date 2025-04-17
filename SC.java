cc24174a23b657da9c90ad239a8ea26437cf0e20
mdani25 <manisekar6911@gmail.com>
2025-04-17 19:09:33 +0530

    String compress program committed

---
+++ SC.java
@@ -0,0 +1,27 @@
+class SC {
+    public static int compress(char[] chars) {
+        int index = 0, i = 0;
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == currentChar) {
+                count++;
+                i++;
+            }
+
+            chars[index++] = currentChar;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index; 
+    }
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(compress(chars)); //OUTPUT:  6
+}}
